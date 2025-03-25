5dde35f248d4d5763a9aa4fbc030f784ed4015fd
nenesihuynu? <motyalove17@mail.ru>
2025-03-25 23:38:48 +0300

    models

--- models/Utils/Utils.h
+++
@@ -1,8 +0,0 @@
-#include "../Employee/Employee.h"
-#include "../Supplier/Supplier.h"
-#include <vector>
-
-void SortEmployees(vector<employee*>& employees);
-void SortSuppliers(vector<supplier*>& suppliers);
-void DeleteSupplier(vector<supplier*>& employees);
-void DeleteEmployee(vector<employee*>& suppliers);
